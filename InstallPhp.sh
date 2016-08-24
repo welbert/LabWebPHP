fe4a75cdbdce610a466093afaeb6bb03a5a0a293
Bruno Novaes Camera Santos <bcamera@users.noreply.github.com>
2016-08-23 22:39:32 -0300

    Abrir link diretamente

--- InstallPhp.sh
+++ InstallPhp.sh
@@ -5,4 +5,5 @@ sudo apt-get install -y php7.0-mysql
 sudo chmod 777 -R /var/www
 sudo service apache2 restart
 echo "<?php phpinfo(); ?>" > /var/www/html/info.php
-echo “Fim da Instalação. Acesse http://localhost/info.php para testar”
+echo “Fim da Instalação. 
+Firefox localhost/info.php

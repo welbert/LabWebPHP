6b47baf2a4460b16e01fc75792deb075bc113fd5
Bruno Novaes Camera Santos <bcamera@users.noreply.github.com>
2016-09-09 21:05:28 -0300

    Inserção do Mbstring
    Remoção do nohup por não ser necessário.

--- InstallPhp.sh
+++ InstallPhp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install -y apache2 php7.0 libapache2-mod-php7.0 mysql-server
-sudo apt-get install -y php7.0-mysql
+sudo apt-get install -y php7.0-mysql php7.0-mbstring
 sudo chmod 777 -R /var/www
 sudo service apache2 restart
 echo "<?php phpinfo(); ?>" > /var/www/html/info.php
 echo “Fim da Instalação. 
-nohup firefox localhost/info.php
+firefox localhost/info.php

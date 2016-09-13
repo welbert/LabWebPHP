6c226e6dac5401d772155045d6f76d244be49178
dnovaes <novaesdigo@gmail.com>
2016-09-13 13:00:31 -0300

    requirements path adjusted with the rewrite mod

--- basic/web/requirements.php
+++ basic/web/requirements.php
@@ -11,7 +11,8 @@
  */
 
 // you may need to adjust this path to the correct Yii framework path
-$frameworkPath = dirname(__FILE__) . '/vendor/yiisoft/yii2';
+// little adjust after the modrewrite activated. added "/.." before the vendor url
+$frameworkPath = dirname(__FILE__) . '/../vendor/yiisoft/yii2';
 
 if (!is_dir($frameworkPath)) {
     echo '<h1>Error</h1>';
